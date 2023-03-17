d0a9544ac2fdda857bd4b8b2edcc3c915dc3e04f
alessiatagliati <alessiatagliati@yahoo.com>
2023-03-17 17:45:24 +0100

    c

--- conversioni_portate.m
+++ conversioni_portate.m
@@ -1,5 +1,6 @@
 %1 l=1 dm^3
 %conversione 60 g/s a l/min
 % densita azoto a 650 K e 200 bar è 90 kg/m^3
+% densita azoto in condizioni ambiente è 1.25 kg/m^3
 60*60*1e-3/(90*1e-3)  % conversione da g/s a l/ min
 3600*(60*1e-3/90)*(90/1.25)   % conversione da g/s a nm^3/h
