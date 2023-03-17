1ef717c745e3a2335f4f63fbda59dac70c8e270a
alessiatagliati <alessiatagliati@yahoo.com>
2023-03-17 16:17:36 +0100

    c

--- Dimensionamento_grano.m
+++ Dimensionamento_grano.m
@@ -5,15 +5,15 @@ P = 1;     % bar
 r_b = (1.152*P^0.768)*1e-3;  % m/s
 
 m_AP = 0.8;     % kg
-m_HTPB = 0.2;    
+m_HTPB = 0.2;     % kg
 rho_AP = 1950;   % kg/m^3
-rho_HTPB = 920;   
+rho_HTPB = 920;    % kg/m^3
 V_AP = m_AP/rho_AP; %m^3
-V_HTPB = m_HTPB/rho_HTPB;
+V_HTPB = m_HTPB/rho_HTPB;  %m^3
 
 rho_mix = (m_AP + m_HTPB)/(V_AP + V_HTPB);  % kg/m^3
 
-m_dot_p = 10*1e-3;   % kg/s
+m_dot_p = 0.0039   % kg/s
 t_b = 20;            % s
 
 A_b = (m_dot_p/(r_b*rho_mix))*1e6    % mm^2
@@ -25,7 +25,7 @@ m_dot_N2 = 50*1e-3;          % kg/s
 T_fin = 650;                 % K
 T_amb = 298.15;              % K
 cp_N2 = 1050;                % J/kgK
-q_N2 = m_dot_N2*cp_N2*(T_fin - T_amb)*1e-3;   % kW
+q_N2 = m_dot_N2*cp_N2*(T_fin - T_amb);   % W
 cp_g = 2363;                 % J/kgK
 T_fl = 2305;                 % K
-m_dot_p = (q_N2/(cp_g*(T_fin - T_amb)));   % g/s
+m_dot_p = q_N2/(cp_g*(T_fl - T_amb));   % kg/s
