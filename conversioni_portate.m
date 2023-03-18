f3b36374ae559ec11ef052e5fae850bad5b0e8f3
alessiatagliati <alessiatagliati@yahoo.com>
2023-03-18 15:49:58 +0100

    c

--- conversioni_portate.m
+++ conversioni_portate.m
@@ -3,6 +3,6 @@
 % densita azoto a 650 K e 200 bar è 90 kg/m^3
 % densita azoto in condizioni ambiente è 1.25 kg/m^3
 mdotN21=60*60*1e-3/(90*1e-3)  % conversione da g/s a l/ min
-mdotN22=3600*(60*1e-3/90)*(200/1)*(293/650)  % conversione da g/s a nm^3/h
+mdotN22=3600*(60*1e-3/90)*(200/1)*(273/650)  % conversione da g/s a nm^3/h
 
-mdotN23=mdotN21*(200/1)*(293/650) % conversione da g/s a Nl/ min
+mdotN23=mdotN21*(200/1)*(273/650) % conversione da g/s a Nl/ min
