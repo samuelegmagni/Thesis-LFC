bb6b9e342faa3d214d9fd83a8c0e7bbdc371a4cd
samuelegmagni <127332370+samuelegmagni@users.noreply.github.com>
2023-03-20 16:20:16 +0100

    ciao

--- Dimensionamento_grano.m
+++ Dimensionamento_grano.m
@@ -1,20 +1,22 @@
 clear
 clc
 
-deltaHreag=-2129.73; %KW/kg dato dal CEA
-% per i prodotti tengo conto di CO,CO2,HCl,H20,N2 e H2 hanno 0
-deltaHprod= 0.32848*(-110.53/(28.01*1e-3))+0.10562*(-393.52/(44.01*1e-3))+0.24448*(-92.31/(36.458*1e-3))+0.20013*(-241.83/(18.02*1e-3)); %KW/kg
-deltaHreaz= deltaHprod-deltaHreag; %KW/kg
-Qcomb=-deltaHreaz; %KW/kg
+% deltaHreag=-2129.73; %KW/kg dato dal CEA
+% % per i prodotti tengo conto di CO,CO2,HCl,H20,N2 e H2 hanno 0
+% deltaHprod= 0.32848*(-110.53/(28.01*1e-3))+0.10562*(-393.52/(44.01*1e-3))+0.24448*(-92.31/(36.458*1e-3))+0.20013*(-241.83/(18.02*1e-3)); %KW/kg
+% deltaHreaz= deltaHprod-deltaHreag; %KW/kg
+% Qcomb=-deltaHreaz*1e3; % W/kg
 
-m_dot_N2 = 50*1e-3;          % kg/s
+m_dot_N2 = 60*1e-3;          % kg/s
 T_fin = 650;                 % K
 T_amb = 298.15;              % K
 cp_N2 = 1050;                % J/kgK
 q_N2 = m_dot_N2*cp_N2*(T_fin - T_amb);   % W
 cp_g = 2363;                 % J/kgK
 T_fl = 2305;                 % K
-m_dot_p = q_N2/(Qcomb*1e3);   % kg/s
+
+f = @(x) m_dot_N2*cp_N2*(T_fin - T_amb) + x*cp_g*(T_fl) - (m_dot_N2 + x) * ( (cp_N2*m_dot_N2)/(m_dot_N2 + x) + (cp_g*x)/(m_dot_N2 + x) )*(T_fin);
+m_dot_p = 1.2*fzero(f,0.5) ;
 
 
 P = 1;     % bar
@@ -34,4 +36,4 @@ A_b = (m_dot_p/(r_b*rho_mix))*1e6;    % mm^2
 d_b = 2*sqrt(A_b/pi);                 % mm
 
 thickness = r_b*t_b*1e3;              % mm
-
+m_prop=A_b*thickness*1e-9*rho_mix;
