f2f4605de1ddebd514edb961469a64002f4f0995
alessiatagliati <alessiatagliati@yahoo.com>
2023-03-20 11:25:37 +0100

    c

--- Dimensionamento_grano.m
+++ Dimensionamento_grano.m
@@ -13,13 +13,13 @@ V_HTPB = m_HTPB/rho_HTPB;  %m^3
 
 rho_mix = (m_AP + m_HTPB)/(V_AP + V_HTPB);  % kg/m^3
 
-m_dot_p = 0.0039   % kg/s
+m_dot_p = 0.0039;   % kg/s
 t_b = 20;            % s
 
-A_b = (m_dot_p/(r_b*rho_mix))*1e6    % mm^2
-d_b = 2*sqrt(A_b/pi)                 % mm
+A_b = (m_dot_p/(r_b*rho_mix))*1e6;    % mm^2
+d_b = 2*sqrt(A_b/pi);                 % mm
 
-thickness = r_b*t_b*1e3              % mm
+thickness = r_b*t_b*1e3;              % mm
 
 m_dot_N2 = 50*1e-3;          % kg/s
 T_fin = 650;                 % K
