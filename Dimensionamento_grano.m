d12128aafbfd25eff4bc8988ad2bdd92d3880459
samuelegmagni <127332370+samuelegmagni@users.noreply.github.com>
2023-03-29 10:38:50 +0200

    bb

--- Dimensionamento_grano.m
+++ Dimensionamento_grano.m
@@ -38,3 +38,99 @@ d_b = 2*sqrt(A_b/pi);                 % mm
 
 thickness = r_b*t_b*1e3;              % mm
 m_prop=A_b*thickness*1e-9*rho_mix;
+
+%%
+
+clear
+clc
+set(0,'DefaultTextFontSize',12);              % Settings for the plot
+set(0,'DefaultAxesFontSize',12);
+set(0,'DefaultLegendInterpreter','Latex');
+set(groot,'DefaultAxesTickLabelInterpreter','Latex');
+set(0,'DefaultTextInterpreter','Latex');
+set(0,'DefaultLegendFontSize',12);
+
+T = 1.2*[300:5:600];
+P = [1:2:100];
+
+T_amb = 298.15;
+cp_g = 2363;                 % J/kgK
+T_fl = 0.8*2305; 
+
+data = nistdata('N2',T,P); 
+
+L_test = 30*1e-3;
+rho = 1.832;
+u = 22.12;
+mom_flux = rho*u^2;      % mom_flux_paper = 896.89;
+
+u_exp = sqrt(mom_flux./(data.Rho*data.Mw));
+
+m_dot_N2 = data.Rho*data.Mw.*u_exp*L_test^2;
+
+cp_N2 = data.Cp/data.Mw;
+
+m_dot_p = zeros(length(T),length(P));
+
+for i = 1 : length(T)
+
+    for m = 1 : length(P)
+
+        f = @(x) m_dot_N2(i,m)*cp_N2(i,m)*(T(i) - T_amb) + x*cp_g*(T_fl) - (m_dot_N2(i,m) + x) * ( (cp_N2(i,m)*m_dot_N2(i,m))/(m_dot_N2(i,m) + x) + (cp_g*x)/(m_dot_N2(i,m) + x) )*(T(i));
+        z = 1.2*fzero(f,0.5) ;
+        m_dot_p(i,m) = z; 
+
+    end
+
+end
+
+figure()
+contourf(data.P*1e-5,data.T,m_dot_N2*1e3); 
+title('Slab 30x30: mass flow rate momentum flux analogy')
+xlabel('Pressure $[bar]$')
+ylabel('Temperature $[K]$')
+c = colorbar;
+c.Label.String = 'm_{dot,N_2} [g/s]';
+
+figure()
+contourf(data.P*1e-5,data.T,m_dot_p*1e3); 
+title('Slab 30x30: mass flow rate momentum flux analogy')
+xlabel('Pressure $[bar]$')
+ylabel('Temperature $[K]$')
+c = colorbar;
+c.Label.String = 'm_{dot,p} [g/s]';
+
+P_prop = 1;     % bar
+r_b = (1.152*P_prop^0.768)*1e-3;  % m/s
+
+m_AP = 0.8;     % kg
+m_HTPB = 0.2;     % kg
+rho_AP = 1950;   % kg/m^3
+rho_HTPB = 920;    % kg/m^3
+V_AP = m_AP/rho_AP; %m^3
+V_HTPB = m_HTPB/rho_HTPB;  %m^3
+
+rho_mix = (m_AP + m_HTPB)/(V_AP + V_HTPB);  % kg/m^3
+t_b = 20;            % s
+
+A_b = (m_dot_p./(r_b*rho_mix))*1e6;    % mm^2
+d_b = 2*sqrt(A_b/pi);                 % mm
+
+thickness = r_b*t_b*1e3;              % mm
+m_prop=A_b*thickness*1e-9*rho_mix;
+
+figure()
+contourf(data.P*1e-5,data.T,d_b); 
+title('Slab 30x30: mass flow rate momentum flux analogy')
+xlabel('Pressure $[bar]$')
+ylabel('Temperature $[K]$')
+c = colorbar;
+c.Label.String = 'd_b [mm]';
+
+figure()
+contourf(data.P*1e-5,data.T,m_prop); 
+title('Slab 30x30: mass flow rate momentum flux analogy')
+xlabel('Pressure $[bar]$')
+ylabel('Temperature $[K]$')
+c = colorbar;
+c.Label.String = 'm_{prop} [kg]';
