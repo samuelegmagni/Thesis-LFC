39399da14cd33a34deb35ed3d1c13417c5e21297
samuelegmagni <127332370+samuelegmagni@users.noreply.github.com>
2023-04-04 15:24:53 +0200

    Update Perdite_di_carico.m

--- Perdite_di_carico.m
+++ Perdite_di_carico.m
@@ -9,12 +9,12 @@ set(groot,'DefaultAxesTickLabelInterpreter','Latex');
 set(0,'DefaultTextInterpreter','Latex');
 set(0,'DefaultLegendFontSize',12);
 T = 298.15;
-P = [7:1:20];
+P = 7:1:20;
 data = nistdata('N2',T,P);
- 
+%% 
 P_c = 7;                             % Pressure in the test chamber [bar]
-d_p_ext = 12*1e-3;                   % Pipe external diameter [m]
-t = 0.7*1e-3;                        % Thickness of the tube  [m]
+d_p_ext = 6*1e-3;                   % Pipe external diameter [m]
+t = 0.9*1e-3;                        % Thickness of the tube  [m]
 d_p_int = d_p_ext - 2*t;             % Pipe internal diameter [m]
 A_int = pi*(d_p_int/2)^2;            % Internal cross sectional area [m^2]
 T_amb = 298.15;                      % Ambient temperature [K]
@@ -23,10 +23,18 @@ cp_N2 = data.Cp/data.Mw;             % Specific heat at constant pressure of Nit
 cv_N2 = data.Cv/data.Mw;             % Specific heat at constant volume of Nitrogen [J/kgK]
 gamma = cp_N2./cv_N2;                % Ratio of specific heats [-]
 mu_N2 = data.mu;                     % Viscosity of Nitrogen [Pa*s]
-L_test = 30*1e-3;                    % Dimension of the square test chamber [m]
-rho = 1.832;                         % Density in full scale facility [kg/m^3]
-u = 22.12;                           % Velocity in full scale facility [m/s]
-mom_flux = rho*u^2;                  % Momentum flux in full scale facility [kg/ms^2]
-m_dot_N2 = sqrt(mom_flux./rho_N2).*rho_N2*L_test^2; % Nitrogen mass flow rate [kg/s]
+% L_test = 30*1e-3;                    % Dimension of the square test chamber [m]
+% rho = 1.832;                         % Density in full scale facility [kg/m^3]
+% u = 22.12;                           % Velocity in full scale facility [m/s]
+% mom_flux = rho*u^2;                  % Momentum flux in full scale facility [kg/ms^2]
+m_dot_N2 = 60*1e-3;                  % Nitrogen mass flow rate [kg/s]
 v_N2 = m_dot_N2./(A_int*rho_N2);     % Velocity of the fluid inside the pipes [m/s]
-Re_N2 = 
+delta_P_inj = 1e-5*40*sqrt(10*P_c*1e5)  
+
+d_inj = 0.5*1e-3;                    % Injector diameter (conical entrance) [m]
+C_d = 0.7;                           % Discharge coefficient
+A_inj = pi*0.25*d_inj^2;             % Injector cross sectional area
+
+c = (gamma*(8314/28)*T).^0.5         % Speed of sound [m/s]
+M = v_N2./c                          % Mach number [-]
+
