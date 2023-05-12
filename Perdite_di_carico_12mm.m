f225f8d9a9e61b5b368c5d8e643e2c22bb026da3
alessiatagliati <alessiatagliati@yahoo.com>
2023-05-12 13:37:36 +0200

    c

--- Perdite_di_carico_12mm.m
+++ Perdite_di_carico_12mm.m
@@ -20,7 +20,7 @@ eps = 0.015*1e-3;                    % Absolute roughness of stainless steel [m]
 eps_rel = eps/d_p_int;               % Relative roughness of stainless steel [-]
 
 T1 = 298;                                       % Temperature downstream the pressure regulator [K]
-P_reg = 70;                                     % Pressure downstream the pressure regulator [bar]
+P_reg = 50;                                     % Pressure downstream the pressure regulator [bar]
 
 T = (floor(T1)-3):0.5:(ceil(T1));
 P = (floor(P_reg)-10):0.1:(ceil(P_reg));
@@ -31,7 +31,7 @@ cp_N2 = data.Cp/data.Mw;             % Specific heat at constant pressure of Nit
 cv_N2 = data.Cv/data.Mw;             % Specific heat at constant volume of Nitrogen [J/kgK]
 gamma_N2 = cp_N2./cv_N2;             % Ratio of specific heats [-]
 mu_N2 = data.mu;                     % Viscosity of Nitrogen [Pa*s]
-m_dot_N2 = 200*1e-3;                  % Nitrogen mass flow rate [kg/s]
+m_dot_N2 = 140*1e-3;                  % Nitrogen mass flow rate [kg/s]
 R = 8314/28;                         % Specific ideal gas constant [J/kgK]
 
 
@@ -360,8 +360,8 @@ T10 = T9;
 
 %% After check valve (point 10) and before injector (point 11)
 
-T = (floor(T10)-40):0.5:(ceil(T10));
-P = (floor(P10)-9):0.1:(ceil(P10));
+T = (floor(T10)-100):0.5:(ceil(T10));
+P = (floor(P10)-6):0.1:(ceil(P10));
 data = nistdata('N2',T,P);
 
 rho_N2 = data.Rho*data.Mw;           % Density of Nitrogen [kg/m^3] 
